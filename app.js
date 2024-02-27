5f3f247f7740ac7d97e592e3bb77c5f5eb8ab582
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-02-27 19:19:39 +0500

    completed

---
+++ app.js
@@ -0,0 +1,207 @@
+/*--------------------- 5-VIDEO------------------------*/
+
+
+/*console.log(1);
+console.log(2);*/
+
+/*--------------------- 6-VIDEO------------------------*/
+
+
+/*let ism = "Davronbek";
+let yosh = 22;
+
+console.log(ism, yosh);
+yosh = 23;
+console.log(yosh);
+
+const familiya = "Anvarov";
+console.log(familiya);
+
+var joy = "Fargona";
+joy = "Fargona tumani";
+console.log(joy);*/
+
+/*--------------------- 9-VIDEO------------------------*/
+
+// string
+/*console.log("Hello world qkqals13wioua!@##$@$");
+let email = "davronbekanvarov23@gmail.com";
+console.log(email);
+// string concatenation
+let firstName = "Davronbek";
+let lastName = "Anvarov";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[6]);
+
+//string Length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+
+let result = fullName.toLowerCase();
+console.log(result);
+
+let index = email.indexOf("@");
+console.log(index);*/
+
+/*--------------------- 10-VIDEO------------------------*/
+
+/*---------ko'p ishlatilinadigan mathodlar ---------- */
+// let email = "davronbekanvarov23@gmail.com";
+// LastIndexof()
+// let result = email.lastIndexOf("a");
+// console.log(result);
+
+// slice()
+
+// let result =email.slice(4,7)
+// console.log(result)
+
+//substr()
+
+// let result =email.substr(4,7)
+// console.log(result)
+
+// replace()
+
+// let result =email.replace('a','e')
+// console.log(result)
+
+// charAt()
+
+// let result = email.charAt();
+// console.log(result);
+
+// toUpperCase() - toLowerCase()
+
+// let result = email.toLocaleUpperCase();
+// console.log(result);
+
+// let result = email.toLocaleLowerCase();
+// console.log(result);
+
+// trim()
+
+// let result =email.trim()
+// console.log(result)
+
+//split()
+
+// let result = email.split('');
+// console.log(result);
+
+/*--------------------- 11-VIDEO------------------------*/
+
+
+// NUMBERS
+
+// let radius = 10;
+// const pi = 3.14;
+
+// console.log(radius, pi);
+
+// MATH operators +,-,*,/,**,%
+// console.log(10/5)
+
+// let result=214%3
+// let result=pi*radius**2
+
+// ORDER ofoperation
+
+// let result=5*(10-8)**2
+
+// let likes = 10;
+
+// likes=likes+1
+// likes++
+// likes--
+// likes=likes+10
+// likes +=10
+// likes -=5
+// likes /=2
+
+// console.log(likes);
+
+// NaN -not a number
+
+// console.log(2 / 'hello');
+// console.log(2 * 'hello');
+
+// let result = 'the block has ' + likes + ' likes'
+
+// console.log(result)
+
+/*--------------------- 12-VIDEO------------------------*/
+
+
+/*const title = "Rastamojka qachon ochiladi ?! yohud Manopoliya Respublikasi !";
+const author = "Lag'mon markazi";
+const like = 35000000;*/
+
+// concatetaion way
+
+// let result = 'bu post nomi ' + title +' '+ author + ' tomonidan yozilgan. ' + 'va bu postda ' + like
+// + 'ta like bor'
+// console.log(result)
+
+// template string way
+
+// let result=`bu post nomi ${title}, ${author} tomonidan yozilgan. Va bu postda ${like} ta like bor`
+
+/*--------------------- 15-VIDEO------------------------*/
+
+// console.log(true, false, "true", "false");
+
+// let email = "davronbekanvarov23@mail.com";
+// let result = email.includes("!");
+// console.log(result);
+
+// let age = 22;
+// console.log(age == 25);//noqatiy tenglik
+// console.log(age!=30)
+// console.log(age>25)
+// console.log(age<25)
+// console.log(age >= 22);
+// console.log(age <= 22);
+
+// let ism = "davronbek";
+// console.log(ism == "davronbek");
+// console.log(ism == "Davronbek");
+// console.log(ism > "Davronbek");
+
+/*--------------------- 16-VIDEO------------------------*/
+// let age = 22;
+
+// kuchsiz taqqoslash
+
+// console.log(age==22)
+// console.log(age=='22')
+// console.log(age != 22);
+// console.log(age!='22')
+
+// kuchli taqqoslash
+
+// console.log(age === 22);
+// console.log(age === "22");
+
+// console.log(age !==22)
+// console.log(age !== "22")
+
+/*--------------------- 17-VIDEO------------------------*/
+
+// let natija = '100';
+// natija=Number(natija)
+// console.log(typeof natija);
+// console.log( natija+1);
+// console.log(natija+1)
+
+// let result=String(15)
+// console.log(typeof result)
+
+// let result=Number('hello')
+// console.log(result)
